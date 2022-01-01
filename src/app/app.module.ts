85c31069af5dfd69af81584cd2ac31821a36e2e4
kalan <kalanin.peto@gmail.com>
2022-01-01 15:54:40 +0100

    feat: new note component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NewNoteComponent } from './dashboard/new-note/new-note.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    NewNoteComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/dashboard/new-note/new-note.component.ts
@@ -0,0 +1,37 @@
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-new-note',
+  templateUrl: './new-note.component.html',
+  styleUrls: ['./new-note.component.scss'],
+  host: { 'class': 'flex justify-center' }
+})
+export class NewNoteComponent implements OnInit {
+  form!: FormGroup;
+  isActive: boolean = false
+
+  @ViewChild('backdrop') backdrop!: ElementRef;
+  @ViewChild('textarea') textarea!: ElementRef;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+
+    this.form = this.fb.group({
+      title: [],
+      content: []
+    });
+  }
+
+  @HostListener('document:mousedown', ['$event'])
+  onGlobalClick(event: { target: any; }): void {
+    if (this.backdrop.nativeElement.contains(event.target) && !this.textarea.nativeElement.contains(event.target)) {
+      this.isActive = false;
+    }
+    if (this.textarea.nativeElement.contains(event.target)) {
+      this.isActive = true;
+    }
+  }
+
+}
