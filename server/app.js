ffaf560bb1278aba98860d6ccf415837226e6d8f
kalan <kalanin.peto@gmail.com>
2022-01-01 12:12:55 +0100

    feat: angular app server integration init

--- server/app.js
+++ server/app.js
@@ -7,10 +7,14 @@ const APP_PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-const distPath = path.join(__dirname, "../public");
+const distPath = path.join(__dirname, "../dist/angular-notes");
 app.use(express.static(distPath));
 
-app.get("/api/ping", function (req, res) {
+app.get("/", (req, res) => {
+  res.sendFile(path.join(distPath, "index.html"));
+});
+
+app.get("/api/ping", (req, res) => {
   res.status(200).json({ response: "pong" });
 });
 
